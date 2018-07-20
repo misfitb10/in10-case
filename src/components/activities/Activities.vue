54232a5e1375a7547db3fa7d21ae674e311e6350
Bilal Hussain <bilal.hussain@incentro.com>
2018-07-20 19:05:59 +0200

    added api based activities component, imported scss, wip = adding classname based on json response

--- src/components/activities/Activities.vue
+++ src/components/activities/Activities.vue
@@ -3,65 +3,83 @@
         <section class="activities">
             <h1 class="activities__head-title">Activiteiten</h1>
 
-            <ol class="activities__list">
-                <li class="activities__list-item is-main">
+            <ol class="activities__list" v-if="articles">
+                <li class="activities__list-item is-main" v-for="article in articles">
                     <div class="activities__secondary-content">
-                        <img class="activities__image" alt="Vaste collectie"
+                        <img class="activities__image"
+                             alt="Vaste collectie"
                              src="/assets/images/introductiecursus-gek-van-surrealisme.png"/>
                     </div>
 
                     <div class="activities__primary-content">
                         <div class="activities__date-and-time">
-                            <span class="activities__date text--date">24 februari 2017</span>
-                            <span class="activities__time">13.00 - 14.30 uur</span>
+                            <span class="activities__date text--date">{{article.date}}</span>
+                            <span class="activities__time">{{article.time}}</span>
                         </div>
 
-                        <h1 class="activities__title">Introductiecursus Gek van surrealisme</h1>
-                        <h2 class="activities__subtitle">Grafiek van Max Klinker</h2>
+                        <h1 class="activities__title">{{article.title}}</h1>
+                        <h2 class="activities__subtitle">{{article.subtitle}}</h2>
 
                         <table class="activities__table-courses">
                             <tbody>
-                            <tr>
-                                <td class="activities__table-courses-subject">Cursussen</td>
-                                <td>&euro; 75,-</td>
-                            </tr>
+                                <tr>
+                                    <td class="activities__table-courses-subject">Cursussen</td>
+                                    <td>&euro; 75,-</td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
                 </li>
 
-                <li class="activities__list-item">
-                    <div class="activities__secondary-content">
-                        <div class="activities__date-and-time">
-                            <span class="activities__date text--date">t/m 5 maart 2017</span>
-                            <span class="activities__time">13.00 - 14.30 uur</span>
-                        </div>
-                    </div>
+                <!--<li class="activities__list-item">-->
+                    <!--<div class="activities__secondary-content">-->
+                        <!--<div class="activities__date-and-time">-->
+                            <!--<span class="activities__date text&#45;&#45;date">t/m 5 maart 2017</span>-->
+                            <!--<span class="activities__time">13.00 - 14.30 uur</span>-->
+                        <!--</div>-->
+                    <!--</div>-->
 
-                    <div class="activities__primary-content">
-                        <div class="activities__info">
-                            <h1 class="activities__title">Kleinkunstig</h1>
-                            <h2 class="activities__subtitle">Kijk en doeboekje</h2>
-                            <p class="activities__category">Kinderen en families</p>
-                        </div>
-                    </div>
-                </li>
+                    <!--<div class="activities__primary-content">-->
+                        <!--<div class="activities__info">-->
+                            <!--<h1 class="activities__title">Kleinkunstig</h1>-->
+                            <!--<h2 class="activities__subtitle">Kijk en doeboekje</h2>-->
+                            <!--<p class="activities__category">Kinderen en families</p>-->
+                        <!--</div>-->
+                    <!--</div>-->
+                <!--</li>-->
             </ol>
 
-            <a href="#" class="activities__show-all-link">Bekijk alle activiteiten</a>
+            <a href="#" class="activities__show-all-link">{{viewAllActivitiesButton}}</a>
         </section>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Vue} from "vue-property-decorator";
+    import axios from 'axios'
 
-    @Component
+    @Component({
+        data: function() {
+            return  {
+                articles: [],
+                viewAllActivitiesButton: ''
+            }
+        },
+        mounted() {
+            axios
+                .get('https://www.bilal.wtf/projects/in10-case/api/api.json')
+                .then(response => {
+                    this.articles = response.data.activities.articles;
+                    this.viewAllActivitiesButton = response.data.activities.moreButton;
+                })
+        }
+    })
     export default class Activities extends Vue {
 
     }
 </script>
 
 <style scoped lang="scss">
-
+    @import "../../scss/base/__base";
+    @import "../../scss/components/_activities";
 </style>
